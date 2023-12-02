ecafa7eb60e490a8e0e344afa4160cd168f489b7
Uzair Ali Nusrat <uzairalinusrat@gmail.com>
2023-12-03 00:42:37 +0500

    Added Image menu cards

---
+++ src/components/ImageCard.js
@@ -0,0 +1,19 @@
+const ImageCard = (props) => {
+    const route = props.route;
+    const image = props.image;
+    const title = props.title;
+
+    return(
+        <figure class="relative max-w-sm cursor-pointer group overflow-hidden border-2 border-gray-500 rounded-2xl">
+            <a href={route}>
+                <img class="rounded-2xl transition-transform duration-300 transform group-hover:scale-105" src={image} alt="image description"/>
+            </a>
+            <figcaption class="absolute text-3xl bottom-6 left-0 right-0 flex items-center justify-center text-black font-bold transition-all duration-300  opacity-70 group-hover:opacity-100 bg-gray-400 ">
+                <p>{title}</p>
+            </figcaption>
+        </figure>
+    
+    );
+}
+
+export default ImageCard;
--- src/pages/AdminHomePage.js
+++ src/pages/AdminHomePage.js
@@ -1,32 +1,21 @@
 import adminProfileImage from '../assets/images/adminProfileimage.png'
 import LoggedInNavbar from '../components/LoggedInNavbar'
-import img3 from "../assets/images/ps5.png"
-import { Fragment } from 'react';
+import ImageCard from "../components/ImageCard"
+import usersImage from "../assets/images/users.png"
+import categoriesImage from "../assets/images/categories.jpg"
 
 const AdminHomePage = () => {
     return(
-        <>
+        <div className="h-screen bg-white">
             <LoggedInNavbar name="Admin" profileImage={adminProfileImage}/>
             <div class="flex items-center space-x-8 p-8">
-                <a href="#" class="block max-w-sm  bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                    <img src={img3}/>
-                    <h5 class="mb-2 text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Ps5</h5>
-                </a>
-            
-                <a href="#" class="block max-w-sm  bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                    <img src={img3}/>
-                    <h5 class="mb-2 text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Ps5</h5>
-                </a>
-
-                <a href="#" class="block max-w-sm  bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                    <img src={img3}/>
-                    <h5 class="mb-2 text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Ps5</h5>
-                </a>
+                <ImageCard route="users" image={usersImage} title="Users"/>
+                <ImageCard route="categories" image={categoriesImage} title="Categories"/>
             </div>
 
 
             
-        </>
+        </div>
     );
 }
 
