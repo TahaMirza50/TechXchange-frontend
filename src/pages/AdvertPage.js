7109aad55aa9243d944a8de6c561f03bfcd6d4ba
TahaMirza50 <tahamirza50@gmail.com>
2024-01-01 16:17:58 +0500

    added auth as redux

--- src/pages/AdvertPage.js
+++ src/pages/AdvertPage.js
@@ -7,7 +7,7 @@ import { useSelector } from "react-redux";
 import useApiPrivate from "../hooks/useAPIPrivate";
 
 const AdvertPage = () => {
-    const {id} = useParams();
+    const { id } = useParams();
 
     const apiPrivate = useApiPrivate();
     const [advert, setAdvert] = useState(null);
@@ -19,19 +19,26 @@ const AdvertPage = () => {
     const categories = useSelector((state) => state.categories.value);
     const [myAd, setMyAd] = useState(false);
     const [categoryName, setCategoryName] = useState("");
+    const [userProfile, setUserProfile] = useState(null);
 
     useEffect(() => {
 
-        const getAdvert = async () => {
+        const getAdvertAndUser = async () => {
             try {
-                console.log(id)
                 const response = await apiPrivate.get(`/advert/get/${id}`);
                 if (response.status === 200) {
                     setAdvert(response.data);
                     if (user.profileID === response.data.userId) {
                         setMyAd(true);
-                    }    
-                    setCategoryName(categories.find((category) => category._id === response.data.categoryId));
+                        setUserProfile(user);
+                    } else {
+                        const responseUser = await apiPrivate.get(`/profile/get/${response.data.userId}`);
+                        if (responseUser.status === 200) {
+                            setUserProfile(responseUser.data);
+                            console.log(responseUser.data);
+                        }
+                    }
+                    setCategoryName(categories?.find((category) => category._id === response.data.categoryId));
                     const date = new Date(response.data.timestamp);
                     setFormattedDate(date.toDateString());
                     setFormattedTime(date.toLocaleTimeString());
@@ -41,14 +48,18 @@ const AdvertPage = () => {
             }
         }
 
-        getAdvert();
+        getAdvertAndUser();
 
-    },[apiPrivate,categories,id,user.profileID])
+    }, [apiPrivate, id, user.profileID, user, categories])
+
+    // useEffect(() => {
+    //     console.log(advert)
+    // }, [advert, categories])
 
     return (
         <div>
             <HomePageNavbar />
-            {advert && <div className="relative flex gap-5 flex-row flex-shrink items-center justify-center  mx-28 my-10">
+            {advert && <div className="relative flex gap-5 flex-row flex-shrink items-start justify-center  mx-28 my-10">
                 <div className="border-2 h-full w-2/3 rounded-md">
                     <div className="flex flex-row h-14 bg-blue-900 rounded-t-md items-center px-5">
                         <svg onClick={() => navigate(-1)} class="w-4 h-4 dark:text-gray-800 text-white hover:text-sky-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
@@ -57,19 +68,19 @@ const AdvertPage = () => {
                         <p className="w-full font-bold text-md text-white text-center">{advert.title}</p>
                     </div>
                     <Carousel leftControl={<button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-                        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-gray-800/30 group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-gray-800/70 group-focus:outline-none">
-                            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-gray-800/30 group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-gray-800/70 group-focus:outline-none">
+                            <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
                             </svg>
                             <span class="sr-only">Previous</span>
                         </span>
                     </button>}
                         rightControl={<button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-gray-800/30 group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-gray-800/70 group-focus:outline-none">
-                                <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-gray-800/30 group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-gray-800/70 group-focus:outline-none">
+                                <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
                                 </svg>
-                                <span class="sr-only">Next</span>
+                                <span className="sr-only">Next</span>
                             </span>
                         </button>}
                         className="h-96 border-b-2 p-2">
@@ -79,7 +90,7 @@ const AdvertPage = () => {
                     </Carousel>
                     <div className="flex flex-row w-full px-5 py-2 items-center justify-between">
                         {!myAd
-                            ? <svg class="w-4 h-4 text-gray-800 dark:text-white hover:text-red-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 21 19">
+                            ? <svg className="w-4 h-4 text-gray-800 dark:text-white hover:text-red-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 21 19">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 4C5.5-1.5-1.5 5.5 4 11l7 7 7-7c5.458-5.458-1.542-12.458-7-7Z" />
                             </svg>
                             : <div></div>}
@@ -89,7 +100,7 @@ const AdvertPage = () => {
                     <p className="px-5">{advert.description}</p>
                     <div className="flex flex-row items-center pt-2 px-5">
                         <p className="font-bold pr-2">Category:</p>
-                        <p>{categoryName.name}</p>
+                        <p>{categoryName?.name}</p>
                     </div>
                     <div className="flex flex-row items-center pt-2 px-5">
                         <p className="font-bold pr-2">Condition:</p>
@@ -100,10 +111,25 @@ const AdvertPage = () => {
                         <p>{advert.location}</p>
                     </div>
                     <div className="h-10" />
-                    <p className="text-gray-400 px-5 pb-2">{formattedDate + " " + formattedTime}</p>
+                    <div className="flex flex-row items-center pb-2 px-5 w-full justify-between">
+                        <p className="text-gray-400">{formattedDate + " " + formattedTime}</p>
+                        <button className="gap-2 text-red-700 font-bold flex-row flex hover:text-red-900 items-center">
+                            <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
+                                <path stroke="currentColor" stroke-linejoin="round" stroke-width="2" d="M6 1v4a1 1 0 0 1-1 1H1m14-4v16a.97.97 0 0 1-.933 1H1.933A.97.97 0 0 1 1 18V5.828a2 2 0 0 1 .586-1.414l2.828-2.828A2 2 0 0 1 5.828 1h8.239A.97.97 0 0 1 15 2Z" />
+                            </svg>
+                            <p>Report Advert</p>
+                        </button>
+                    </div>
                 </div>
                 <div className="border-2 h-full w-1/3 rounded-md">
-
+                    <div className="flex flex-row h-14 bg-blue-900 rounded-t-md items-center px-5" />
+                    {userProfile &&
+                        <div className="bg-gray-200 w-full m-5 p-2 rounded-md flex flex-row">
+                            <svg className="w-1/4 h-full text-blue-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+                            </svg>
+                            <p>{userProfile.firstName} {userProfile.lastName}</p>
+                        </div>}
                 </div>
             </div>}
             <Footer />
