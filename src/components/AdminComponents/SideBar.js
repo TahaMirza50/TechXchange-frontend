abe6d3691794c36e2077da4f3b6a3bb806d8f955
Uzair Ali <uzairalinusrat@gmail.com>
2024-01-04 20:29:12 +0500

    changed to link

--- src/components/AdminComponents/SideBar.js
+++ src/components/AdminComponents/SideBar.js
@@ -1,4 +1,4 @@
-import { NavLink, useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const SideBar = (props) => {
     const location = useLocation();
@@ -8,7 +8,7 @@ const SideBar = (props) => {
             <div className="rounded-r py-4 bg-blue-100 dark:bg-gray-800">
                 <ul className="h-screen relative flex flex-col space-y-2 font-medium">
                     <li>
-                        <NavLink
+                        <Link
                             to="/admin-dashboard"
                             className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white dark:hover:bg-gray-700 group ${location.pathname === '/admin-dashboard' ? 'bg-blue-500 text-white' : ''}`}
                         >
@@ -16,10 +16,10 @@ const SideBar = (props) => {
                                 <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
                             </svg>
                             <span className="flex-1 ms-3 whitespace-nowrap">Users</span>
-                        </NavLink>
+                        </Link>
                     </li>
                     <li>
-                        <NavLink
+                        <Link
                             to="categories"
                             className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white group ${location.pathname === '/admin-dashboard/categories' ? 'bg-blue-500 text-white' : ''}`}
                         >
@@ -27,10 +27,10 @@ const SideBar = (props) => {
                                 <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z"/>
                             </svg>
                             <span className="flex-1 ms-3 whitespace-nowrap">Categories</span>
-                        </NavLink>
+                        </Link>
                     </li>
                     <li>
-                        <NavLink
+                        <Link
                             to="advertisements"
                             className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white ${location.pathname === '/admin-dashboard/advertisements' ? 'bg-blue-500 text-white' : ''}`}
                         >
@@ -38,10 +38,10 @@ const SideBar = (props) => {
                                 <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
                             </svg>
                             <span className="flex-1 ms-3 whitespace-nowrap">Advertisements</span>
-                        </NavLink>
+                        </Link>
                     </li>
                     <li>
-                        <NavLink
+                        <Link
                             to="reports"
                             className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white ${location.pathname === '/admin-dashboard/reports' ? 'bg-blue-500 text-white' : ''}`}
                         >
@@ -49,7 +49,7 @@ const SideBar = (props) => {
                                 <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z"/>
                             </svg>
                             <span className="flex-1 ms-3 whitespace-nowrap">Reports</span>
-                        </NavLink>
+                        </Link>
                     </li>
                     <li className="absolute bottom-20">
                         <button onClick={props.handleLogOut}>
